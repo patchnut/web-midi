2f27f0e3114eb1a5eafe152ad8987ea507be8129
Mendelt Siebenga <msiebenga@gmail.com>
2020-11-09 22:38:00 +0100

    initial outline

---
+++ src/lib.rs
@@ -0,0 +1,25 @@
+pub struct MidiAccess {
+
+}
+
+impl MidiAccess {
+    pub async fn get_access() -> Self {
+        MidiAccess {}
+    }
+
+    pub fn inputs(&self) -> Vec<MidiInput> {
+        todo!()
+    }
+
+    pub fn outputs(&self) -> Vec<MidiOutput> {
+        todo!();
+    }
+}
+
+pub struct MidiInput {
+
+}
+
+pub struct MidiOutput {
+
+}
