e372b85a1c16b2f220eb3f214dc2f1da4430fa5e
Mendelt Siebenga <msiebenga@gmail.com>
2020-11-09 23:21:29 +0100

    Get midi access, try to enumerate inputs and outputs

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,40 @@
-pub struct MidiAccess {
+use web_sys::{MidiOptions, console};
+use wasm_bindgen_futures::JsFuture;
 
+pub struct MidiAccess {
+    access: web_sys::MidiAccess
 }
 
 impl MidiAccess {
     pub async fn get_access() -> Self {
-        MidiAccess {}
+        let window = web_sys::window().expect("no global `window` exists");
+
+        let access: web_sys::MidiAccess = JsFuture::from(
+            window.navigator()
+                .request_midi_access_with_options(MidiOptions::new().sysex(true).software(true))
+                .unwrap(),
+        )
+        .await
+        .unwrap()
+        .into();
+
+        Self { access }
     }
 
     pub fn inputs(&self) -> Vec<MidiInput> {
-        todo!()
+        for entry in js_sys::try_iter(&self.access.inputs()).unwrap().unwrap() {
+            console::log_1(&entry.unwrap());
+        }
+
+        Vec::new()
     }
 
     pub fn outputs(&self) -> Vec<MidiOutput> {
-        todo!();
+        for entry in js_sys::try_iter(&self.access.inputs()).unwrap().unwrap() {
+            console::log_1(&entry.unwrap());
+        }
+
+        Vec::new()
     }
 }
 
