0c73f74acbbefda7e9672609041168c7a79ebced
Mendelt Siebenga <msiebenga@gmail.com>
2020-11-25 22:49:06 +0100

    use log crate to log to console in example

--- examples/list_interfaces/src/lib.rs
+++ examples/list_interfaces/src/lib.rs
@@ -1,35 +1,25 @@
 use async_std::task;
+use log::{info, Level};
 use wasm_bindgen::prelude::*;
-use wasm_bindgen_futures::JsFuture;
 use web_midi::MidiAccess;
 
 #[wasm_bindgen]
-extern "C" {
-    // Use `js_namespace` here to bind `console.log(..)` instead of just `log(..)`
-    #[wasm_bindgen(js_namespace = console)]
-    fn log(s: &str);
-}
-
-macro_rules! console {
-   ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
-}
-
-#[wasm_bindgen]
 pub fn main_js() -> Result<(), JsValue> {
+    console_log::init_with_level(Level::Debug).expect("Unable to initialize logging");
 
     task::block_on(async {
         let window = web_sys::window().expect("no global `window` exists");
         let document = window.document().expect("should have a document on window");
-        let body = document.body().expect("document should have a body");
+        let _body = document.body().expect("document should have a body");
 
         let access = MidiAccess::get_access(window.navigator()).await;
 
         for input in access.inputs() {
-            console!("input {} {} {}", input.name().unwrap(), input.manufacturer().unwrap(), input.version().unwrap())
+            info!("input {} {} {}", input.name().unwrap(), input.manufacturer().unwrap(), input.version().unwrap());
         }
 
         for output in access.outputs() {
-            console!("output {} {} {}", output.name().unwrap(), output.manufacturer().unwrap(), output.version().unwrap())
+            info!("output {} {} {}", output.name().unwrap(), output.manufacturer().unwrap(), output.version().unwrap());
         }
     });
 
